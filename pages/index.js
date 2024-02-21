0eea0836a00cd680106727b47bffbe46565789e4
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2024-02-21 22:29:05 +0000

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <h1>Fui para os bosques viver de livre vontade.<br />
         Para sugar todo o tutano da vida.<br />
-        Para aniquilar tudo o que não era vida e não para quando morrer,<br />
+        Para aniquilar tudo o que não era vida<br />
+        e não para quando morrer,<br />
         descobrir que não vivi.</h1>
 }
 
