ca0d7b2243f3a2a02b78401049086d17719aa56c
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2024-02-23 22:23:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 function Home() {
-  return <h1>Fui para os bosques viver de livre vontade.<br />
-    Para sugar todo o tutano da vida.<br />
-    Para aniquilar tudo o que não era vida<br />
-    e não para quando morrer,<br />
-    descobrir que não vivi.</h1>
+  return (
+    <h1>
+      Fui para os bosques viver de livre vontade.
+      <br />
+      Para sugar todo o tutano da vida.
+      <br />
+      Para aniquilar tudo o que não era vida
+      <br />
+      e não para quando morrer,
+      <br />
+      descobrir que não vivi.
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
