52ad9a1581685125ca1d7f4358b7ee7598fe96df
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2024-05-08 22:40:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,40 @@
 function Home() {
-    return <p>
-        O universo possui um começo, mas não<br />
-        um fim. Infinito. As estrelas também tem<br />
-        um começo, mas seu poder resulta<br />
-        própria sua destruição. Finito. A história<br />
-        tem mostrado que aqueles com sabedoria<br />
-        são os maiores tolos de todos. Peixes que<br />
-        vivem no mar não sabem nada sobre a<br />
-        terra. Se eles tivessem essa sabedoria,<br />
-        eles, por sua vez, seriam destruídos.<br />
-        Humanos excederem a velocidade da luz<br />
-        é mais tolo do que peixes começarem a<br />
-        viver em terra. Essas são palavras<br />
-        destinadas àqueles que ousam desafiar o<br />
-        último aviso de Deus… Uma epígrafe<br />
-        para sua teimosia.<br /><br />
-        <b>Maho Hiyajo – Steins;Gate Zero.</b>
+  return (
+    <p>
+      O universo possui um começo, mas não
+      <br />
+      um fim. Infinito. As estrelas também tem
+      <br />
+      um começo, mas seu poder resulta
+      <br />
+      própria sua destruição. Finito. A história
+      <br />
+      tem mostrado que aqueles com sabedoria
+      <br />
+      são os maiores tolos de todos. Peixes que
+      <br />
+      vivem no mar não sabem nada sobre a
+      <br />
+      terra. Se eles tivessem essa sabedoria,
+      <br />
+      eles, por sua vez, seriam destruídos.
+      <br />
+      Humanos excederem a velocidade da luz
+      <br />
+      é mais tolo do que peixes começarem a
+      <br />
+      viver em terra. Essas são palavras
+      <br />
+      destinadas àqueles que ousam desafiar o
+      <br />
+      último aviso de Deus… Uma epígrafe
+      <br />
+      para sua teimosia.
+      <br />
+      <br />
+      <b>Maho Hiyajo – Steins;Gate Zero.</b>
     </p>
+  );
 }
 
-export default Home
+export default Home;
