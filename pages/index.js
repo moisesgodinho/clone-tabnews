bb8b47c86f0f2723d8c1114984968e6916de13ca
Moises Godinho <56728202+moisesgodinho@users.noreply.github.com>
2025-07-08 22:16:08 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <h1>
+      You just seem oh-so-far <br />
+      Past the stars and past my heart
+      <br />
+      I can almost feel you and I start
+      <br />
+      To close my eyes and sing a song
+      <br />
+      I will keep singing cause I'm wrong
+      <br />
+      But you were singing with me all along.
+    </h1>
+  );
+}
+
+export default Home;
